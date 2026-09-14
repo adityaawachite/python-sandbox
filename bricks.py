7d8e674e877580900b7e80326017b21a14cf54dd
Aditya Gorkshnath Awachite <adigorkshnathawachite@gmail.com>
2026-09-14 23:40:36 +0530

    Add files via upload

---
+++ bricks.py
@@ -0,0 +1,6 @@
+brick_cost = 8
+bricks_per_sqrft = 12
+labour_charge = 15
+material_cost = bricks_per_sqrft + brick_cost
+total_cost_per_sqrft = material_cost + labour_charge
+print("total constructiom cost per square fit :"total_cost_per_sqrft," rupees")
